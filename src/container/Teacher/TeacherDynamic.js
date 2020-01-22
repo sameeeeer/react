455c20c46bd27905f46dba53643ed2e7def49364
sameeeeer <lastking.me01@gmail.com>
2020-01-22 13:34:55 +0545

    Class

--- src/container/Teacher/TeacherDynamic.js
+++ src/container/Teacher/TeacherDynamic.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Teacher.css'
+import axios from 'axios'
 
 class TeacherDynamic extends React.Component{
+   state = {
+      teacher_data : []
+   }
+ componentDidMount(){
+    axios.get("http://localhost:3000/users/single/"+this.props.match.params.id).then(res =>{
+this.setState({teacher_data : res.data})
+ })
+ }
  render(){
  return(
- <div className="teachereach">{this.props.match.params.id}</div>
+ <div className="teachereach">
+    {/* Id of the user <b>{this.props.match.params.id}</b></div> */}
+ <div>{this.state.teacher_data.fname}</div>
+ <div>{this.state.teacher_data.lname}</div>
+ </div>
+
     )
     }
    }
